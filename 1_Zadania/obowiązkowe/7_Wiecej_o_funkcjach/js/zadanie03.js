1be9d826da308d2b51f467bb5ebcf75ce12b0e51
Bodzioch Lubor <lubor.bodzioch@microsoft.wsei.edu.pl>
2019-12-08 12:34:02 +0100

    08.12

---
+++ 1_Zadania/obowiązkowe/7_Wiecej_o_funkcjach/js/zadanie03.js
@@ -0,0 +1,15 @@
+
+czesc();
+
+
+function czesc(){
+    console.log("Czesc");
+}
+
+var b = function(){
+    console.log("Witaj");
+};
+
+b();
+
+czesc();
--- 1_Zadania/obowiązkowe/8_DOM_Wyszukiwanie_elementow/js/app.js
+++ 1_Zadania/obowiązkowe/8_DOM_Wyszukiwanie_elementow/js/app.js
@@ -2,5 +2,34 @@ document.addEventListener("DOMContentLoaded", function () {
     /*
     Poniżej napisz kod rozwiązujący zadania. Odpowiedzi oddzielaj komentarzami.
      */
+    const title1 = document.querySelector("div .title");
+
+    function getDataAnimation(element){
+        return element.getAttribute("data-animation");
+    }
+
+    console.log(getDataAnimation(title1));
+
+    const home = document.querySelector("#home");
+    const home2 = document.getElementById("home");
+    const liElement = document.querySelector("li[data-direction]");
+    const block = document.querySelector(".block");
+    
+    //zadanie 2
+    const list = document.querySelectorAll("nav li");
+    const paragraphs = document.querySelectorAll("div p");
+    const divs = document.querySelectorAll("article div");
+    
+    console.log(list);
+    console.log(paragraphs);
+    console.log(divs);
+
+    //zadanie 3
+    const article = document.querySelector("article.first");
+    let result = article.getElementsByTagName("h1");
+    console.log(result.length);
+
 
 });
+
+
