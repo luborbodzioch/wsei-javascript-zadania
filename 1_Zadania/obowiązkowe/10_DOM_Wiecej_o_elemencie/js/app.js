4bb6b4c632ec29a9ac58494360f60ee97e166953
Bodzioch Lubor <lubor.bodzioch@microsoft.wsei.edu.pl>
2019-12-22 12:38:53 +0100

    kolejny123

--- 1_Zadania/obowiązkowe/10_DOM_Wiecej_o_elemencie/js/app.js
+++ 1_Zadania/obowiązkowe/10_DOM_Wiecej_o_elemencie/js/app.js
@@ -3,4 +3,29 @@ document.addEventListener("DOMContentLoaded", function () {
     Poniżej napisz kod rozwiązujący zadania
      */
 
+
+     //zadanie 0
+    const ex5 = document.getElementsByClassName("ex5")[0];
+
+    const ex5children = ex5.children[1].children;
+
+    for(let i = 0; i < ex5children.length; i++){
+        if(i % 2 ==0){
+        ex5children[i].style.backgroundColor = "green";
+        };
+
+        if(i % 3 ==0){
+            ex5children[i].style.textDecoration = "underline";
+        };
+
+
+    }
+
+    ex5children[4].classList.add('big');
+
+    
+    
+
+
+
 });
--- 1_Zadania/obowiązkowe/9_DOM_Element/js/app.js
+++ 1_Zadania/obowiązkowe/9_DOM_Element/js/app.js
@@ -7,6 +7,114 @@ document.addEventListener("DOMContentLoaded", function () {
 
     /*
     Poniżej napisz kod rozwiązujący zadania
-     */
+    */
+
+    //zadanie 0
+    
+
+
+    function getDatasInfo(elements){
+        
+        let arr = [];
+        for(let i = 0; i < elements.length; i++){
+            arr.push(elements[i].getAttribute("data-color"));
+        };
+
+        return arr;
+
+    }
+
+    getDatasInfo(links);
+
+    //zadanie 1
+
+    console.log(childElements);
+    console.log(homeElement);
+    console.log(banner);
+    console.log(blocks);
+    console.log(links);
+
+    for(let i = 0; i < childElements.length; i++){
+       console.log(childElements[i].tagName);
+       console.log(childElements[i].classList);
+    }
+
+    for(let i = 0; i < links.length; i++){
+        console.log(links[i].tagName);
+        console.log(links[i].classList);
+     }
+
+     
+     for(let i = 0; i < blocks.length; i++){
+        console.log(blocks[i].tagName);
+        console.log(blocks[i].classList);
+     }
+ 
+    //zadanie 2
+
+    for(let i = 0; i < blocks.length; i++){
+        console.log(blocks[i].innerHTML);
+        console.log(blocks[i].outerHTML);
+        blocks[i].innerHTML = 'Nowa wartosc diva o klasie blocks';
+    }
+        //innerHTML zwraca wartos tagu
+        //outerHTML zwraca wartosc tagu razem z tagiem
+        
+
+    //zadanie 3
+
+    var mainFooter = document.getElementById("mainFooter");
+
+    function getId(element){
+
+        console.log(element.id);
+        return element.id;
+
+    }
+
+    getId(mainFooter);
+
+    //zadanie 4
+
+    function getTags(elements){
+
+        let arr = [];
+        console.log(elements);
+        for(let i =0;i< elements.length; i++){
+            arr.push(elements[i].tagName);
+        }
+        console.log(arr);
+        return arr;
+    }
+
+    getTags(childElements);
+
+    //zadanie 5
+
+    function getClassInfo(element){
+       
+        var classes = [];
+        
+        for(let i = 0; i<element.classList.length ; i++){
+            classes.push(element.classList[i]);
+        }
+        console.log(classes);
+        return classes;
+
+    }
+
+    getClassInfo(banner);
+
+    //zadanie 6
+
+    const liElements = document.querySelectorAll('nav li');
+    const setDataDirection = elements => elements.forEach(el => el.getAttribute('data-direction') && el.setAttribute('data-direction','top'));
+
+    setDataDirection([...liElements]);
+
 
 });
+
+
+
+
